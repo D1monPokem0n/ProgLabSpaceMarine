905bac2d3fe59b494f8a399ecc4b938b32f70d37
D1monPokem0n <ditch725@gmail.com>
2023-03-22 12:21:13 +0300

    Functional Interface for Space Marine builder and creator

---
+++ src/main/java/ru/prog/itmo/spacemarine/builder/SpaceMarineCheckSettable.java
@@ -0,0 +1,5 @@
+package ru.prog.itmo.spacemarine.builder;
+@FunctionalInterface
+public interface SpaceMarineCheckSettable {
+    boolean checkSet() throws InvalidSpaceMarineValueException, CreateCancelledException;
+}
