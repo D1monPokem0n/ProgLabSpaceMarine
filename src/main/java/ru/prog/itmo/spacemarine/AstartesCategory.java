f95441a70207fd3e93092efb750cadb541e5f25f
D1monPokem0n <ditch725@gmail.com>
2023-03-22 11:59:20 +0300

    CTRL+ALT+L

--- src/main/java/ru/prog/itmo/spacemarine/AstartesCategory.java
+++ src/main/java/ru/prog/itmo/spacemarine/AstartesCategory.java
@@ -1,12 +1,15 @@
 package ru.prog.itmo.spacemarine;
 
+
 public enum AstartesCategory {
-    SCOUT("Scout"),
-    LIBRARIAN("Librarian"),
     CHAPLAIN("Chaplain"),
-    APOTHECARY("Apothecary");
-    private String name;
-    AstartesCategory(String name){
+    LIBRARIAN("Librarian"),   // chaplain > librarian > apothecary > scout
+    APOTHECARY("Apothecary"),
+    SCOUT("Scout");
+
+    private final String name;
+
+    AstartesCategory(String name) {
         this.name = name;
     }
 
