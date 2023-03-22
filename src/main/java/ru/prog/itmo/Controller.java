7a2778f302c90b0969d4e83a91a3ba918c93f4ed
D1monPokem0n <ditch725@gmail.com>
2023-03-22 12:13:27 +0300

    separated Command map to a new class, added console argument

--- src/main/java/ru/prog/itmo/Controller.java
+++ src/main/java/ru/prog/itmo/Controller.java
@@ -1,33 +1,34 @@
 package ru.prog.itmo;
 
-import ru.prog.itmo.command.*;
 
-import java.util.HashMap;
 
 public class Controller {
-    private ProgramState programState;
+    private final ProgramState programState;
     private Storage storage;
-    private final HashMap<String, Command> commandList;
+    private ConsoleArgument argument;
+    private final CommandMap commandMap;
     public Controller(Storage storage){
-        commandList = new HashMap<>();
-        commandList.put("add", new AddCommand(storage));
-        commandList.put("add_if_min", new AddIfMinCommand(storage));
-        commandList.put("clear", new ClearCommand(storage));
-        commandList.put("execute_script", new ExecuteScriptCommand());
-        commandList.put("exit", new ExitCommand());
-        commandList.put("remove_any_by_chapter", new RemoveAnyByChapterCommand(storage));
-        commandList.put("max_by_melee_weapon", new MaxByMeleeWeaponCommand(storage));
-        commandList.put("help", new HelpCommand());
-        commandList.put("history", new HistoryCommand());
-        commandList.put("info", new InfoCommand(storage));
-        commandList.put("print_field_descending_health", new PrintFieldDescendingHealthCommand(storage));
-        commandList.put("remove_by_id", new RemoveByIdCommand(storage));
-        commandList.put("remove_greater", new RemoveGreaterCommand(storage));
-        commandList.put("save", new SaveCommand(storage));
-        commandList.put("show", new ShowCommand(storage));
-        commandList.put("update", new UpdateCommand(storage));
+        programState = new ProgramState(true);
+        argument = new ConsoleArgument();
+        commandMap = new CommandMap(storage, programState, argument);
     }
     public void executeCommand(String commandName){
-        commandList.get(commandName).execute();
+        commandMap.getCommandHashMap().get(commandName).execute();
+    }
+
+    public CommandMap getCommandMap() {
+        return commandMap;
+    }
+
+    public ConsoleArgument getArgument() {
+        return argument;
+    }
+
+    public void setArgument(ConsoleArgument argument) {
+        this.argument = argument;
+    }
+
+    public ProgramState getProgramState() {
+        return programState;
     }
 }
