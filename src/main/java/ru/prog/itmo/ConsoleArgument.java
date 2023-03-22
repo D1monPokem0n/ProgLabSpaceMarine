ae12c9bac35f062e8270cf6064078e9fac2b14da
D1monPokem0n <ditch725@gmail.com>
2023-03-22 12:10:14 +0300

    Added class for Console Argument

---
+++ src/main/java/ru/prog/itmo/ConsoleArgument.java
@@ -0,0 +1,13 @@
+package ru.prog.itmo;
+
+public class ConsoleArgument {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setArgument(String value) {
+        this.value = value;
+    }
+}
