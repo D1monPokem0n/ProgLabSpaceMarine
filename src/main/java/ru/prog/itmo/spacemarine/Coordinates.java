ae6c4b69b8079ffba5d334a978073f513a689bba
D1monPokem0n <ditch725@gmail.com>
2023-03-22 12:13:55 +0300

    CTRL+ALT+L

--- src/main/java/ru/prog/itmo/spacemarine/Coordinates.java
+++ src/main/java/ru/prog/itmo/spacemarine/Coordinates.java
@@ -3,13 +3,14 @@ package ru.prog.itmo.spacemarine;
 public class Coordinates {
     private Float x; //Поле не может быть null
     private Double y; //Максимальное значение поля: 431, Поле не может быть null
-    public Coordinates(Float x, Double y){
+
+    public Coordinates(Float x, Double y) {
         this.x = x;
         this.y = y;
     }
 
     @Override
     public String toString() {
-        return "Coordinate_x:"+x+",Coordinate_y:"+y;
+        return "Coordinate_x:" + x + ",Coordinate_y:" + y;
     }
 }
