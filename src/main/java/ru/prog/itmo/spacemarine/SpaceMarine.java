bd646718497520c6a49a81e6ebb0306b6e59d99f
D1monPokem0n <ditch725@gmail.com>
2023-03-22 12:18:45 +0300

    compareTo, getters, setters

--- src/main/java/ru/prog/itmo/spacemarine/SpaceMarine.java
+++ src/main/java/ru/prog/itmo/spacemarine/SpaceMarine.java
@@ -1,8 +1,10 @@
 package ru.prog.itmo.spacemarine;
 
+import ru.prog.itmo.spacemarine.builder.InvalidSpaceMarineValueException;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
-public class SpaceMarine {
+public class SpaceMarine implements Comparable<SpaceMarine> {
     private long id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
@@ -14,18 +16,19 @@ public class SpaceMarine {
     private Chapter chapter; //Поле может быть null
     private static long lastId;
 
-    public SpaceMarine(String name,
+    public SpaceMarine(long id,
+                       String name,
                        Coordinates coordinates,
+                       java.time.LocalDateTime creationDate,
                        int health,
                        long heartCount,
                        AstartesCategory category,
                        MeleeWeapon meleeWeapon,
                        Chapter chapter) {
-        lastId++;
-        this.id = lastId;
+        this.id = id;
         this.name = name;
         this.coordinates = coordinates;
-        creationDate = java.time.LocalDateTime.now();
+        this.creationDate = creationDate;
         this.health = health;
         this.heartCount = heartCount;
         this.category = category;
@@ -33,13 +36,58 @@ public class SpaceMarine {
         this.chapter = chapter;
     }
 
+    public static long getLastId() {
+        return lastId;
+    }
+
     public static void setLastId(long id) {
         lastId = id;
     }
 
+    public static void incrementLastId() throws InvalidSpaceMarineValueException {
+        if (lastId == Long.MAX_VALUE) throw new InvalidSpaceMarineValueException("Вы превысили размеры long.");
+        lastId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public AstartesCategory getCategory() {
+        return category;
+    }
+
+    public Chapter getChapter() {
+        return chapter;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public long getHeartCount() {
+        return heartCount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public MeleeWeapon getMeleeWeapon() {
+        return meleeWeapon;
+    }
+
     @Override
     public int hashCode() {
-        return 7 * super.hashCode() + 13 * Objects.hash(id, name, category, meleeWeapon, chapter);
+        return 7 * super.hashCode() + 13 * Objects.hash(name, heartCount, category, meleeWeapon, chapter);
     }
 
     @Override
@@ -47,24 +95,37 @@ public class SpaceMarine {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SpaceMarine other = (SpaceMarine) o;
-        if (other.id == this.id) return true;
         return Objects.equals(name, other.name)
-                && category == other.category
+                && Objects.equals(heartCount, other.heartCount)
                 && meleeWeapon == other.meleeWeapon
+                && category == other.category
                 && Objects.equals(chapter, other.chapter);
     }
 
     @Override
     public String toString() {
         return super.toString()
-                +"[Id:" + id
+                + "[Id:" + id
                 + ",name:" + name
                 + ",coordinates:[" + coordinates.toString() + "]"
-                + ",CreationDate:"+creationDate.toString()
+                + ",CreationDate:" + creationDate.toString()
                 + ",health:" + health
                 + ",heartCount:" + heartCount
                 + ",category:" + category.getName()
                 + ",meleeWeapon:" + meleeWeapon.getName()
                 + ",chapter:[" + chapter.toString() + "]" + "]";
+    }
+
+    @Override
+    public int compareTo(SpaceMarine o) {
+        if (chapter.compareTo(o.chapter) != 0)
+            return chapter.compareTo(o.chapter);
+        if (category.compareTo(o.category) != 0)
+            return category.compareTo(o.category);
+        if (meleeWeapon.compareTo(o.meleeWeapon) != 0)
+            return meleeWeapon.compareTo(o.meleeWeapon);
+        if (heartCount != o.heartCount)
+            return heartCount > o.heartCount ? 1 : -1;
+        return name.compareTo(o.name);
     }
 }
